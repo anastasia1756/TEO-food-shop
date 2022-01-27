f6ba45768c9ee0834f5a5784378f60dc92faf99a
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-01-27 05:26:39 +0200

    feat: add popup modal

--- index.js
+++ index.js
@@ -4,3 +4,30 @@ let swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+// Timer
+let timeleft = 10;
+let popModalTimer = setInterval(() => {
+  console.log(timeleft);
+  if (timeleft <= 0) {
+    clearInterval(popModalTimer);
+    document.querySelector("[data-modal]").classList.remove("is-hidden");
+  }
+  const secRef = document.querySelector(".seconds");
+  timeleft -= 1;
+  if (timeleft < 0) {
+    return;
+  }
+  secRef.textContent = "0" + timeleft;
+}, 1000);
+
+// Modal
+(() => {
+  const refs = {
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
+
+  refs.closeModalBtn.addEventListener("click", () => {
+    refs.modal.classList.add("is-hidden");
+  });
+})();
