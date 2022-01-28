719873e67f912a3d8973429cc430c68baa1f03b1
anastasia1756 <anastasiia7kolomiiets@gmail.com>
2022-01-28 02:01:00 +0200

    feat: add data for table and load more

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let swiper = new Swiper(".mySwiper", {
 // Timer
 let timeleft = 10;
 let popModalTimer = setInterval(() => {
-  console.log(timeleft);
   if (timeleft <= 0) {
     clearInterval(popModalTimer);
     document.querySelector("[data-modal]").classList.remove("is-hidden");
@@ -62,6 +61,82 @@ form.addEventListener("submit", (e) => {
       form.reset();
     });
 });
-// fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((response) => response.json())
-//   .then((json) => console.log(json));
+
+// Table
+const fetchUsers = async () => {
+  const formRef = document.querySelector(".table");
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  const markup = users
+    .map(({ id, username, name, email, phone }) => {
+      return `
+    
+  <tr class="table__data">
+    <td class="table__item-bold">${id}</td>
+    <td>${name}</td>
+    <td>${username}</td>
+    <td>${email}</td>
+    <td>${phone}</td>
+  </tr> 
+  `;
+    })
+    .join("");
+  formRef.insertAdjacentHTML("beforeend", markup);
+};
+const moreUsers = () => {
+  const btnRef = document.querySelector(".table__btn");
+  btnRef.addEventListener("click", () => {
+    const formRef = document.querySelector(".table");
+    const moreUsers = [
+      {
+        id: 11,
+        name: "Ana",
+        username: "Toro",
+        email: "ana@ukr.net",
+        phone: "0634256781",
+      },
+      {
+        id: 12,
+        name: "Alexa",
+        username: "Bingo",
+        email: "al@gmail.com",
+        phone: "0676856789",
+      },
+      {
+        id: 13,
+        name: "Petra",
+        username: "Toro",
+        email: "ana@ukr.net",
+        phone: "0634256781",
+      },
+      {
+        id: 14,
+        name: "John",
+        username: "Braun",
+        email: "al@gmail.com",
+        phone: "0676856789",
+      },
+    ];
+    const markup = moreUsers
+      .map(({ id, username, name, email, phone }) => {
+        return `
+  <tr class="table__data">
+    <td class="table__item-bold">${id}</td>
+    <td>${name}</td>
+    <td>${username}</td>
+    <td>${email}</td>
+    <td>${phone}</td>
+  </tr> 
+  `;
+      })
+      .join("");
+    formRef.insertAdjacentHTML("beforeend", markup);
+    if (moreUsers.length >= 4) {
+      btnRef.setAttribute("disabled", "disabled");
+    }
+  });
+};
+fetchUsers();
+moreUsers();
